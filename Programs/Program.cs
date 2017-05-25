36ac90b2607af70b6ef9b8a19d85b0d9993399b2
Jeam <reroll58@mail.ru>
2017-05-25 22:31:16 +0500

    Add ExchangeTwoVariables method

--- Programs/Program.cs
+++ Programs/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Programs
 {
     class Program
     {
+        static void ExchangeTwoVariables(int a, int b )
+        {
+            int c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("{0} {1}", a, b);
+        }
         static void Main(string[] args)
         {
+            ExchangeTwoVariables(1, 2);
         }
     }
 }
