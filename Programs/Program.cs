d87617bad615ccdca9a2590dad677f97e0e554f3
Jeam <reroll58@mail.ru>
2017-05-26 17:22:18 +0500

    Add Reverse3DigitsNumber

--- Programs/Program.cs
+++ Programs/Program.cs
@@ -11,9 +11,21 @@ namespace Programs
             b = c;
             Console.WriteLine("{0} {1}", a, b);
         }
+
+        static void Reverse3DigitsNumber(int number)
+        {
+            int n0, n1, n2;
+            n0 = number % 10;
+            number /= 10;
+            n1 = number % 10;
+            n2 = number / 10;
+            Console.WriteLine(n2 + n1 * 10 + n0 * 100);
+        }
+
         static void Main(string[] args)
         {
             ExchangeTwoVariables(1, 2);
+            Reverse3DigitsNumber(450);
         }
     }
 }
